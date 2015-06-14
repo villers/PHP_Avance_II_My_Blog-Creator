e9c1c333edf703a18c8fcead603f50c11063692d
viller_m <mickael.villers@epitech.eu>
2015-06-14 16:48:23 +0200

    Création des models

--- app/User.php
+++ app/User.php
@@ -20,16 +20,34 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
     protected $table = 'users';
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = ['name', 'email', 'password'];
-
-    /**
      * The attributes excluded from the model's JSON form.
      *
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
+
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
+
+    public function role()
+    {
+        return $this->belongsTo('App\Role');
+    }
+
+    public function isAdmin()
+    {
+        return $this->role->slug == 'admin';
+    }
+
+    public function isNotUser()
+    {
+        return $this->role->slug != 'user';
+    }
 }
