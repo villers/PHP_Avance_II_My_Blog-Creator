d17e08a46821931b1e73e0cb1e6ec6d591bbe056
viller_m <mickael.villers@epitech.eu>
2015-06-22 10:21:56 +0200

    Ajout de la table blog

---
+++ database/migrations/2015_06_22_081101_create_blogs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 255);
+            $table->text('summary');
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('blogs');
+    }
+}
